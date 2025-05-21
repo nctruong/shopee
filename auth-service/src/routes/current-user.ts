83ca43b6923ca20ca4b769210e360004836187e8
truong <willnguyen2971990@gmail.com>
2025-05-21 16:09:33 +0700

    get current user

--- auth-service/src/routes/current-user.ts
+++ auth-service/src/routes/current-user.ts
@@ -1,8 +1,21 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
-router.get("/api/users/currentUser", (req, res) => {
-    res.send('hi there')
+router.get("/api/users/currentUser", (req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+        res.send({currentUser: null})
+        return;
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send( {currentUser: payload})
+    } catch (err) {
+        res.status(401).send({ currentUser: null });
+    }
+
 })
 
 export { router as currentUserRouter }
