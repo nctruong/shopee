9e3528f53cfe52337ba13fc4c8e1e0fcb011e571
truong <willnguyen2971990@gmail.com>
2025-05-21 17:37:30 +0700

    some middlewares

--- auth-service/src/middlewares/handle-errors.ts
+++ auth-service/src/middlewares/handle-errors.ts
@@ -1,20 +1,18 @@
-import { Request, Response, NextFunction } from 'express'
-import {ValidationError, validationResult} from "express-validator";
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
-export const handleErrors = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-        const formattedErrors = errors.array().map((err: ValidationError) => {
-            console.log(JSON.stringify(err));
-            if (err.type === 'field') {
-                return { message: err.msg, field: err.path };
-            }
-            return { message: err.msg };
-        });
-        res.status(400).send({ errors: formattedErrors });
-        return
+export const handleErrors = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
 
-    next()
-}
+    console.error(err);
+    res.status(400).send({
+        errors: [{ message: 'Something went wrong' }]
+    });
+};
--- auth-service/src/routes/current-user.ts
+++ auth-service/src/routes/current-user.ts
@@ -1,20 +1,11 @@
 import express, {Request, Response} from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user"
 
 const router = express.Router();
 
-router.get("/api/users/currentUser", (req: Request, res: Response) => {
-    if (!req.session?.jwt) {
-        res.send({currentUser: null})
-        return;
-    }
-
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        res.send( {currentUser: payload})
-    } catch (err) {
-        res.status(401).send({ currentUser: null });
-    }
+router.get("/api/users/currentUser", currentUser, (req: Request, res: Response) => {
+    res.send({currentUser: req.currentUser || null })
 
 })
 
