c81a3c5f11b2966fe0ed84c2aafdb3c147de8e8d
truong <willnguyen2971990@gmail.com>
2025-05-23 18:16:21 +0700

    integrate tailwindcss

--- client-service/pages/_app.js
+++ client-service/pages/_app.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css';
 
 export default ({ Component, pageProps }) => {
     return <Component {...pageProps} />
--- client-service/pages/auth/signup.js
+++ client-service/pages/auth/signup.js
@@ -18,7 +18,7 @@ export default () => {
         }
     }
 
-    return <form onSubmit={onSubmit}>
+    return <form onSubmit={onSubmit} className="flex">
         <h1>Sign up</h1>
         <div className={"form-group"}>
             <label htmlFor="email">Email</label>
@@ -31,13 +31,24 @@ export default () => {
             <input value={password} onChange={e => setPassword(e.target.value)} className="form-control" id="password"
                    type="password" placeholder="Password"/>
         </div>
-        <div className={"alert alert-danger"}>
-            <ul className={"list-group my-0"}>
-                {errors && errors.map(err => (
-                    <li>{err.message}</li>
-                ))}
-            </ul>
-        </div>
+        {errors.length > 0 && (
+            <div className={"alert alert-danger"}>
+                <ul className={"list-group my-0"}>
+                    {errors.map(err => (
+                        <li>{err.message}</li>
+                    ))}
+                </ul>
+            </div>
+        )}
         <button type="submit btn btn-primary">Sign up</button>
+
+        <div
+            className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+            <img className="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo"/>
+            <div>
+                <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+                <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+            </div>
+        </div>
     </form>
 }
