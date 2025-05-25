f110a23b3cf046fd06be20761d745d251d85d0d3
truong <willnguyen2971990@gmail.com>
2025-05-25 13:27:30 +0700

    refactor code

--- client-service/pages/index.js
+++ client-service/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
-    return <h1>hello abcde</h1>
+import buildClient from '../api/build-client'
+
+const LandingPage = ({currentUser}) => {
+    return <h1>Landing Page {JSON.stringify(currentUser)}</h1>
 }
+
+LandingPage.getInitialProps = async context => {
+    console.log('on server')
+
+    const { data } = await buildClient(context).get('/api/users/currentUser')
+    console.log(data)
+    return data
+}
+
+export default LandingPage;
